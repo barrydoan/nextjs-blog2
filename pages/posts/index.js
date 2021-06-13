9589349947aca88b8b2a4816f9970ae53b060491
Barry Doan <barry.n.doan@gmail.com>
2021-06-13 09:09:25 -0400

    Add all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,4 +1,30 @@
-function AllPostsPage() {
+import AllPosts from "../../components/posts/all-posts";
 
+const DUMMY_POSTS = [
+  {slug: 'getting-started-with-nextjs',
+    title: 'Getting started with Nextjs' ,
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJs is React framwork for production',
+    date: '2022-02-12'},
+  {slug: 'getting-started-with-nextjs2',
+    title: 'Getting started with Nextjs' ,
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJs is React framwork for production',
+    date: '2022-02-12'},
+  {slug: 'getting-started-with-nextjs3',
+    title: 'Getting started with Nextjs' ,
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJs is React framwork for production',
+    date: '2022-02-12'},
+  {slug: 'getting-started-with-nextjs4',
+    title: 'Getting started with Nextjs' ,
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJs is React framwork for production',
+    date: '2022-02-12'}
+
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />
 }
 export default AllPostsPage;
