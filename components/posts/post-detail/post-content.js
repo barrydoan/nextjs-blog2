34f0f8feb202d02879f7e0d48148e724a5a56717
Barry Doan <barry.n.doan@gmail.com>
2021-06-16 07:52:07 -0400

    add new post

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,6 +2,8 @@ import PostHeader from "./post-header";
 import classes from './post-content.module.css';
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 function PostContent(props) {
 
@@ -28,6 +30,10 @@ function PostContent(props) {
         </div>
       }
       return <p>{paragraph.children}</p>
+    },
+    code(code) {
+      const { language, value } = code;
+      return <SyntaxHighlighter style={atomDark} language={language} children={value} />
     }
   };
 
